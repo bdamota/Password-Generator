fadf36a9dcf0d0fabc443e2f37ee40adf02d627f
LYNN DAMOTA <bdamota0506@gmail.com>
2020-06-15 01:56:22 -0700

    cleaned up JS

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var confirmNumeric;
 var confirmSpecial;
 //Generate Password Function 
 function generatePassword() {
-    var confirmLength = (prompt("How many characters would you like your password to contain? Choose between 8-128 characters."));
+  var confirmLength = (prompt("How many characters would you like your password to contain? Choose between 8-128 characters."));
       if (confirmLength >= 8 && confirmLength <= 128) {
           alert(`Your password will have ${confirmLength} characters.`);
       } 
@@ -20,7 +20,6 @@ function generatePassword() {
           alert("Password length must be between 8-128 characters. Try again.");
           var confirmLength = (prompt("How many characters would you like your password to contain? Choose between 8-128 characters."));
       }
-
 // Determine parameters of password
   var confirmSelection = alert("Choose at least one of the following character types to include: 'SPECIAL', 'NUMERIC', 'LOWERCASE', 'UPPERCASE.'");
   var confirmSpecial = prompt("Enter 'YES' to include 'SPECIAL' characters. Leave blank to exclude.");
@@ -28,50 +27,48 @@ function generatePassword() {
   var confirmLowerCase = prompt("Enter 'YES' to include 'LOWERCASE' characters. Leave blank to exclude.");
   var confirmUpperCase = prompt("Enter 'YES' to include 'UPPERCASE' characters. Leave blank to exclude."); 
 
-  while (confirmSpecial === "" && confirmNumeric === ""  &&  confirmLowerCase === "" && confirmUpperCase === "") {
-    alert("You must choose at least one character type.");
-      var confirmSpecial = prompt("Enter 'YES' to include 'SPECIAL' characters. Leave blank to exclude.");
-      var confirmNumeric = prompt("Enter 'YES' to include 'NUMERIC' characters. Leave blank to exclude.");    
-      var confirmLowerCase = prompt("Enter 'YES' to include 'LOWERCASE' characters. Leave blank to exclude.");
-      var confirmUpperCase = prompt("Enter 'YES' to include 'UPPERCASE' characters. Leave blank to exclude."); 
-  }
-  
+     while (confirmSpecial === "" && confirmNumeric === "" && confirmLowerCase === ""  && confirmUpperCase === "") {
+        alert("You must choose at least one character type.");
+        var confirmSpecial = prompt("Enter 'YES' to include 'SPECIAL' characters. Leave blank to exclude.");
+        var confirmNumeric = prompt("Enter 'YES' to include 'NUMERIC' characters. Leave blank to exclude.");    
+        var confirmLowerCase = prompt("Enter 'YES' to include 'LOWERCASE' characters. Leave blank to exclude.");
+        var confirmUpperCase = prompt("Enter 'YES' to include 'UPPERCASE' characters. Leave blank to exclude."); 
+      }
 //Assign action to password parameters 
-  var passwordCharacters = []
+  var passwordCharacters = [];
       
-  if (confirmSpecial === "YES" || confirmSpecial === "yes")  {
-    passwordCharacters = passwordCharacters.concat(specialChar);
-  }
+     if (confirmSpecial === "YES" || confirmSpecial === "yes")  {
+        passwordCharacters = passwordCharacters.concat(specialChar);
+      }
 
-  if (confirmNumeric === "YES" || confirmNumeric === "yes")  {
-    passwordCharacters = passwordCharacters.concat(number);
-  }
-    
-  if (confirmLowerCase === "YES" || confirmLowerCase === "yes") {
-    passwordCharacters = passwordCharacters.concat(alphaLower);
-  }
+      if (confirmNumeric === "YES" || confirmNumeric === "yes")  {
+        passwordCharacters = passwordCharacters.concat(number);
+      }
+        
+      if (confirmLowerCase === "YES" || confirmLowerCase === "yes") {
+        passwordCharacters = passwordCharacters.concat(alphaLower);
+      }
 
-  if  (confirmUpperCase === "YES" || confirmUpperCase === "yes") {
-    passwordCharacters = passwordCharacters.concat(alphaUpper);
-  };
+      if  (confirmUpperCase === "YES" || confirmUpperCase === "yes") {
+        passwordCharacters = passwordCharacters.concat(alphaUpper);
+      }
 
-    console.log(passwordCharacters)
-
+        console.log(passwordCharacters);
 // Empty string to be filled based on for loop selecting random characters from the array
 var randomPassword = ""
     
-for (var i = 0; i < confirmLength; i++) {
-  randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-  console.log(randomPassword)
-}
-  return randomPassword;
-};
+    for (var i = 0; i < confirmLength; i++) {
+      randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+      console.log(randomPassword)
+    }
+      return randomPassword;
+    }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
 };
